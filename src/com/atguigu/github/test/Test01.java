9da726bf1932a5ad31eb32a77db24838d277611f
xiaolu1331 <54427153+xiaolu1331@users.noreply.github.com>
2019-08-24 10:50:09 +0000

    to local
    to local

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello,github服务器");
 		System.out.println("hello,github服务器02");
+		System.out.println("github服务器  response to local");
 	}
 
 }
