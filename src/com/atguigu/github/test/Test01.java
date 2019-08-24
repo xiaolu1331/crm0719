44e643b982ddcc9563d303b565e5448d43ae2115
xiaolu1331 <54427153+xiaolu1331@users.noreply.github.com>
2019-08-24 12:10:50 +0000

    2
    2

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -6,6 +6,7 @@ public class Test01 {
 		System.out.println("hello,github服务器");
 		System.out.println("hello,github服务器02");
 		System.out.println("github服务器  response to local");
+		System.out.println("github服务器  response to local  02");
 	}
 
 }
